ec88e2395de0a50d49959e7d809b03bc09328253
Fernanda <engcivil.fernandasouza@gmail.com>
2022-03-07 12:18:08 -0300

    alteracao importCSS

--- src/utils/importCSS/index.js
+++ src/utils/importCSS/index.js
@@ -1,7 +1,25 @@
 import { createElement } from '../index.js';
 
 export const importCSS = (path) => {
-  const $link = createElement('link', { rel: 'stylesheet', href: `./src/${path}/styles.css` });
   const $head = document.querySelector('head');
-  $head.appendChild($link);
+
+  if (path.includes('pages/')) {
+    const $link = createElement('link', { rel: 'stylesheet', href: `./src/${path}/styles.css` });
+    return $head.appendChild($link);
+  }
+  if (path.includes('shared/')) {
+    const $link = createElement('link', { rel: 'stylesheet', href: `./src/components/${path}/styles.css` });
+    return $head.appendChild($link);
+  }
+  if (path.includes('modals/')) {
+    const $link = createElement('link', { rel: 'stylesheet', href: `./src/components/${path}/styles.css` });
+    return $head.appendChild($link);
+  }
+  if (path.includes('/')) {
+    const $link = createElement('link', { rel: 'stylesheet', href: path });
+    return $head.appendChild($link);
+  }
+
+  const $link = createElement('link', { rel: 'stylesheet', href: `./src/components/${path}/styles.css` });
+  return $head.appendChild($link);
 };
